4db50f7ea24e6a4f5c93354324580f217d1a2c85
Brandon Donnelson <branflake2267@gmail.com>
2013-02-06 15:39:36 -0800

    Initial project for KitchenSink

---
+++ gwtp-samples/gwtp-sample-kitchensink/src/main/java/com/gwtplatform/samples/kitchensink/client/application/ApplicationView.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.kitchensink.client.application;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.Style.Visibility;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.inject.Inject;
+import com.gwtplatform.mvp.client.ViewImpl;
+
+/**
+ * This is the top-level view of the application. Every time another presenter
+ * wants to reveal itself, {@link ApplicationView} will add its content of the
+ * target inside the {@code mainContantPanel}.
+ */
+public class ApplicationView extends ViewImpl implements ApplicationPresenter.MyView {
+    public interface Binder extends UiBinder<Widget, ApplicationView> {
+    }
+
+    public final Widget widget;
+
+    @UiField
+    SimplePanel mainContentPanel;
+    @UiField
+    Element loadingMessage;
+
+    @Inject
+    public ApplicationView(final Binder binder) {
+        widget = binder.createAndBindUi(this);
+    }
+
+    @Override
+    public Widget asWidget() {
+        return widget;
+    }
+
+    @Override
+    public void setInSlot(Object slot, Widget content) {
+        if (slot == ApplicationPresenter.TYPE_SetMainContent) {
+            mainContentPanel.setWidget(content);
+        } else {
+            super.setInSlot(slot, content);
+        }
+    }
+
+    @Override
+    public void showLoading(boolean visibile) {
+        loadingMessage.getStyle().setVisibility(visibile ? Visibility.VISIBLE : Visibility.HIDDEN);
+    }
+}
---
+++ gwtp-samples/gwtp-sample-kitchensink/src/main/java/com/gwtplatform/samples/kitchensink/client/gin/ClientModule.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2011 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.kitchensink.client.gin;
+
+import com.gwtplatform.mvp.client.annotations.DefaultPlace;
+import com.gwtplatform.mvp.client.annotations.ErrorPlace;
+import com.gwtplatform.mvp.client.annotations.UnauthorizedPlace;
+import com.gwtplatform.mvp.client.gin.AbstractPresenterModule;
+import com.gwtplatform.mvp.client.gin.DefaultModule;
+import com.gwtplatform.mvp.client.proxy.DefaultPlaceManager;
+import com.gwtplatform.samples.kitchensink.client.application.ApplicationModule;
+import com.gwtplatform.samples.kitchensink.client.place.NameTokens;
+
+public class ClientModule extends AbstractPresenterModule {
+    @Override
+    protected void configure() {
+        install(new DefaultModule(DefaultPlaceManager.class));
+        install(new ApplicationModule());
+
+        // DefaultPlaceManager Constants
+        bindConstant().annotatedWith(DefaultPlace.class).to(NameTokens.homePage);
+        bindConstant().annotatedWith(ErrorPlace.class).to(NameTokens.homePage);
+        bindConstant().annotatedWith(UnauthorizedPlace.class).to(NameTokens.homePage);
+    }
+}
