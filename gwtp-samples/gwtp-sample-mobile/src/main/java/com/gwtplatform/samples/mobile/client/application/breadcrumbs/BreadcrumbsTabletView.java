f825c25c495d60831b45189c28705b0df029c875
Brandon Donnelson <branflake2267@gmail.com>
2013-03-19 18:34:11 -0700

    import cleanup

--- gwtp-samples/gwtp-sample-mobile/src/main/java/com/gwtplatform/samples/mobile/client/application/breadcrumbs/BreadcrumbsTabletView.java
+++ gwtp-samples/gwtp-sample-mobile/src/main/java/com/gwtplatform/samples/mobile/client/application/breadcrumbs/BreadcrumbsTabletView.java
@@ -24,7 +24,6 @@ import com.google.gwt.user.client.ui.InlineLabel;
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.user.client.ui.IsWidget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
 import com.gwtplatform.mvp.client.proxy.PlaceManager;
