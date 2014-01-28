57a2f70d66c55a23d67fa71d6adccb864d5ed185
Christopher Viel <viel.christopher@gmail.com>
2014-01-28 15:17:22 -0500

    Added support for sub-resources
    Former-commit-id: c00e1e319f9c28e8141e3a12d6141d79283ccb69

--- src/main/java/com/gwtplatform/carstore/shared/rest/ResourcesPath.java
+++ src/main/java/com/gwtplatform/carstore/shared/rest/ResourcesPath.java
@@ -17,7 +17,7 @@
 package com.gwtplatform.carstore.shared.rest;
 
 public class ResourcesPath {
-    public static final String CAR = "/cars";
+    public static final String CARS = "/cars";
     public static final String MANUFACTURER = "/manufacturers";
     public static final String RATING = "/ratings";
     public static final String SESSION = "/session";
