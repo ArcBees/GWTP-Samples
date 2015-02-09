d261ce42fb093e58d13ff83341f73577294fafa2
Jean-Christophe Lariviere <jclariviere@arcbees.com>
2015-02-09 17:12:03 -0500

    Remove unused classes related to CarStoreElementLocator

--- carstore/src/test/java/com/gwtplatform/carstore/cucumber/CucumberModule.java
+++ carstore/src/test/java/com/gwtplatform/carstore/cucumber/CucumberModule.java
@@ -20,19 +20,15 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.pagefactory.ElementLocatorFactory;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.gwtplatform.carstore.cucumber.application.BasePage;
-import com.gwtplatform.carstore.cucumber.util.CarStoreElementLocatorFactory;
-import cucumber.api.guice.CucumberScopes;
 import cucumber.runtime.java.guice.ScenarioScoped;
 
 public class CucumberModule extends AbstractModule {
     @Override
     protected void configure() {
-        bind(ElementLocatorFactory.class).to(CarStoreElementLocatorFactory.class).in(CucumberScopes.SCENARIO);
         requestStaticInjection(BasePage.class);
     }
 
