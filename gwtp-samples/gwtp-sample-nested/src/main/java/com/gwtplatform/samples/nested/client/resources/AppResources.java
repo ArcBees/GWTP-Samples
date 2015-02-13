a1a7d2ea0f22008ec52389c59347ccdcf92745af
jasonlemay <jason.lemay@arcbees.com>
2015-02-13 10:35:23 -0500

    Style and clean GWTP Sample Nested

---
+++ gwtp-samples/gwtp-sample-nested/src/main/java/com/gwtplatform/samples/nested/client/resources/AppResources.java
@@ -0,0 +1,25 @@
+package com.gwtplatform.samples.nested.client.resources;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface AppResources extends ClientBundle {
+    interface Normalize extends CssResource {
+    }
+
+    interface Style extends CssResource {
+        String label_error();
+
+        String container();
+
+        String box();
+
+        String links();
+    }
+
+    @Source("css/normalize.gss")
+    Normalize normalize();
+    
+    @Source("css/style.gss")
+    public Style style();
+}
