6b42ce625ef21f3f0c271f2ff658ad66280325f3
Maxime Mériouma-Caron <maxime.meriouma@arcbees.com>
2015-03-06 10:45:27 -0500

    Added GWTP Polymer basic POC

---
+++ gwtp-samples/gwtp-sample-polymer/src/main/java/com/gwtplatform/samples/polymer/client/application/ApplicationPresenter.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2015 ArcBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gwtplatform.samples.polymer.client.application;
+
+import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
+import com.gwtplatform.mvp.client.HasUiHandlers;
+import com.gwtplatform.mvp.client.Presenter;
+import com.gwtplatform.mvp.client.View;
+import com.gwtplatform.mvp.client.annotations.ProxyStandard;
+import com.gwtplatform.mvp.client.presenter.slots.NestedSlot;
+import com.gwtplatform.mvp.client.proxy.PlaceManager;
+import com.gwtplatform.mvp.client.proxy.Proxy;
+import com.gwtplatform.mvp.shared.proxy.PlaceRequest;
+import com.gwtplatform.samples.polymer.client.place.NameTokens;
+
+public class ApplicationPresenter
+        extends Presenter<ApplicationPresenter.MyView, ApplicationPresenter.MyProxy>
+        implements ApplicationUiHandlers {
+    interface MyView extends View, HasUiHandlers<ApplicationUiHandlers> {
+    }
+
+    @ProxyStandard
+    interface MyProxy extends Proxy<ApplicationPresenter> {
+    }
+
+    public static final NestedSlot SLOT_MAIN_CONTENT = new NestedSlot();
+
+    private final PlaceManager placeManager;
+
+    @Inject
+    ApplicationPresenter(EventBus eventBus,
+                         MyView view,
+                         MyProxy proxy,
+                         PlaceManager placeManager) {
+        super(eventBus, view, proxy, RevealType.RootLayout);
+
+        this.placeManager = placeManager;
+
+        view.setUiHandlers(this);
+    }
+
+    @Override
+    public void goToPageOne() {
+        goTo(NameTokens.PAGE1);
+    }
+
+    @Override
+    public void goToPageTwo() {
+        goTo(NameTokens.PAGE2);
+    }
+
+    private void goTo(String nameToken) {
+        placeManager.revealPlace(new PlaceRequest.Builder().nameToken(nameToken).build());
+    }
+}
