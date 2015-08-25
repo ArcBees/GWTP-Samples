c9b7ca51d65a5efbcc6876185b0aa182aa04c3ab
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-08-25 12:41:02 -0400

    Add a comment that warn the user : bindSlot(...) is not usable with a Composite.

--- gwtp-samples/gwtp-sample-tab/src/main/java/com/gwtplatform/samples/tab/client/application/ApplicationView.java
+++ gwtp-samples/gwtp-sample-tab/src/main/java/com/gwtplatform/samples/tab/client/application/ApplicationView.java
@@ -78,6 +78,7 @@ public class ApplicationView extends ViewImpl implements ApplicationPresenter.My
         tabPanel.changeTab(tab, tabData, historyToken);
     }
 
+    //Cannot use bindSlot(...) with a Composite
     @Override
     public void setInSlot(Object slot, IsWidget content) {
         if (slot == ApplicationPresenter.SLOT_TAB_CONTENT) {
